04b15c6903b7317431f44ea7d5b0743ca1672f53
Bianca Paccola <bianca.paccola@gmail.com>
2023-03-12 12:16:46 -0300

    instalações e congifurações

--- projeto-LAMA/src/data/BaseDatabase.ts
+++ projeto-LAMA/src/data/BaseDatabase.ts
@@ -1,7 +1,4 @@
-import knex from "knex";
-import Knex   from "knex";
-
-
+import knex, { Knex } from "knex";
 export abstract class BaseDatabase {
 
     private static connection: Knex | null = null;
